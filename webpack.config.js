7bf6bf2e87fd7d58e9e22f6a0509007be4863408
Gleb Dronchenko <mariocomp93@ya.ru>
2026-08-17 14:41:11 +0300

    main: preparing for public page

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
         path: path.resolve(__dirname, "dist"),
         filename: "[name].[contenthash].js",
         clean: true,
+        publicPath: process.env.PUBLIC_PATH || "/",
     },
     resolve: {
         extensions: [".ts", ".tsx", ".js"],
